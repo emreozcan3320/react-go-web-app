5510743d470c1014f3e708e98e24daa024cf1e76
Emre Ozcan <emre@codeshake.io>
2020-04-26 18:20:03 +0300

    gorilla/mux router added and hello wold web example created

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
-package main 
+package main
 
-import "fmt" 
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func main() { 
-    fmt.Println("Hello World!")
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func main() {
+	// Print console when server starting
+	fmt.Println("Go Server Starting...") 
+
+	// Init Router
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Route Handler / Endpoints
+	router.HandleFunc("/", homeLink).Methods("GET")
+
+	// Run the server and log.Fatal if it fail
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
